947ee38304ad7bd9da8b5e3bf7c791eb8a007402
Volker Killesreiter <killesre@gmail.com>
2018-05-23 17:17:45 +0200

    added tests for nested

--- tests/src/FunctionalJavascript/ParagraphsFeaturesDeleteConfirmationTest.php
+++ tests/src/FunctionalJavascript/ParagraphsFeaturesDeleteConfirmationTest.php
@@ -60,17 +60,40 @@ class ParagraphsFeaturesDeleteConfirmationTest extends ParagraphsFeaturesJavascr
     // Paragraph is gone.
     $this->assertSession()->elementNotExists('xpath', '//div[@id="field-paragraphs-0-item-wrapper"]');
 
-    // 2) Disable setting to show confirmation on remove action.
+    // 2) Setup nested.
+    $this->setupNestedParagraphSettings();
+    $this->drupalGet("node/add/$content_type");
+    $session = $this->getSession();
+    $page = $session->getPage();
+
+    // 2a) Add nested paragraph.
+    $page->find('xpath', '//input[@data-drupal-selector="field-paragraphs-test-nested-add-more"]')->click();
+    $this->assertSession()->assertWaitOnAjaxRequest();
+
+    // 2b) Trigger confirmation.
+    $session->evaluateScript("jQuery('div[data-drupal-selector=\"edit-field-paragraphs-1-subform-field-paragraphs-0-top\"]').find('.paragraphs-features__delete-confirm').trigger('mousedown')");
+
+    $is_element_visible = $session->evaluateScript("jQuery('div[id^=\"field-paragraphs-1-subform-field-paragraphs-0-item-wrapper\"]').find('.paragraphs-features__delete-confirmation').length === 1");
+    $this->assertEquals(TRUE, $is_element_visible, 'Confirmation form should be visible in subform.');
+
+    // 2b) Make sure correct paragraph form is removed.
+    $session->evaluateScript("jQuery('div[id^=\"field-paragraphs-1-subform-field-paragraphs-0-item-wrapper\"]').find('.paragraphs-features__delete-confirmation__remove-button').trigger('mousedown')");
+    $this->assertSession()->assertWaitOnAjaxRequest();
+
+    $is_element_gone = $session->evaluateScript("jQuery('div[id^=\"field-paragraphs-1-subform-field-paragraphs-0-item-wrapper\"]').length === 0");
+    $this->assertEquals(TRUE, $is_element_gone, 'Nested paragraph subform should be gone.');
+
+    // 3) Disable setting to show confirmation on remove action.
     $this->disableDeleteConfirmation($content_type);
 
     $this->drupalGet("node/add/$content_type");
     $session = $this->getSession();
     $page = $session->getPage();
 
-    // 2a) custom remove button is not present.
+    // 3a) Custom remove button is not present.
     $this->assertSession()->elementNotExists('xpath', '//button[@class="paragraphs-features__delete-confirm"]');
 
-    // 2b) Instant removal.
+    // 3b) Instant removal.
     $page->find('xpath', '//button[contains(@class, "paragraphs-dropdown-toggle")]')->click();
     $page->find('xpath', '//input[@data-drupal-selector="field-paragraphs-0-remove"]')->click();
     $this->assertSession()->assertWaitOnAjaxRequest();
@@ -92,6 +115,23 @@ class ParagraphsFeaturesDeleteConfirmationTest extends ParagraphsFeaturesJavascr
     // Have a default paragraph, it simplifies the clicking on the edit page.
     $this->config('core.entity_form_display.node.' . $content_type . '.default')
       ->set('content.field_paragraphs.settings.default_paragraph_type', 'test_1')
+      ->set('content.field_paragraphs.settings.add_mode', 'button')
+      ->save();
+
+    $this->drupalGet($currentUrl);
+  }
+
+  /**
+   * Setup paragraphs field for nested paragraph.
+   *
+   */
+  protected function setupNestedParagraphSettings() {
+    $currentUrl = $this->getSession()->getCurrentUrl();
+
+    // Default paragraph and edit mode
+    $this->config('core.entity_form_display.paragraph.test_nested.default')
+      ->set('content.field_paragraphs.settings.default_paragraph_type', 'test_1')
+      ->set('content.field_paragraphs.settings.edit_mode', 'open')
       ->save();
 
     $this->drupalGet($currentUrl);
