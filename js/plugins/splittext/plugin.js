c5fd8eae18c2128817ff612d9e4696a1fc48f750
Volker Killesreiter <killesre@gmail.com>
2020-06-02 16:29:25 +0200

    fix next row selection for add in between

--- js/plugins/splittext/plugin.js
+++ js/plugins/splittext/plugin.js
@@ -177,7 +177,7 @@
     var $originalRow = $(originalRow);
 
     // Set "cut" data ot new paragraph.
-    var $newRow = $originalRow.next();
+    var $newRow = $originalRow.nextAll('tr[class*="paragraph-type--"]').first();
 
     // Build regex for search.
     var fieldSelector = tmpObject.originalEditorWrapperSelector.replace(/-[0-9]+-/, '-[0-9]+-');
