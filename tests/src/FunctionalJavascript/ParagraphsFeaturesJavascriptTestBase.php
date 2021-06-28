4b3ee3d4e072491ba76e8e0c3eedde23c4b42918
Christian Fritsch <chr.fritsch@gmx.net>
2021-06-28 16:55:48 +0200

    fix: scrolling

--- tests/src/FunctionalJavascript/ParagraphsFeaturesJavascriptTestBase.php
+++ tests/src/FunctionalJavascript/ParagraphsFeaturesJavascriptTestBase.php
@@ -159,4 +159,21 @@ abstract class ParagraphsFeaturesJavascriptTestBase extends WebDriverTestBase {
     return $this->getSession()->getPage()->find('xpath', '//*[@data-drupal-selector="edit-field-paragraphs-' . $paragraph_index . '"]//textarea')->getAttribute('id');
   }
 
+  /**
+   * Scroll element with defined css selector in middle of browser view.
+   *
+   * @param string $cssSelector
+   *   CSS Selector for element that should be centralized.
+   */
+  public function scrollElementInView($cssSelector) {
+    $this->getSession()
+      ->executeScript('
+        var viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+        var element = jQuery(\'' . addcslashes($cssSelector, '\'') . '\');
+        var scrollTop = element.offset().top - (viewPortHeight/2);
+        var scrollableParent = jQuery.isFunction(element.scrollParent) ? element.scrollParent() : [];
+        if (scrollableParent.length > 0 && scrollableParent[0] !== document && scrollableParent[0] !== document.body) { scrollableParent[0].scrollTop = scrollableParent[0].scrollTop + scrollTop - scrollableParent.offset().top } else { window.scroll(0, scrollTop); };
+      ');
+  }
+
 }
--- tests/src/FunctionalJavascript/ParagraphsFeaturesSplitTextTest.php
+++ tests/src/FunctionalJavascript/ParagraphsFeaturesSplitTextTest.php
@@ -32,7 +32,6 @@ class ParagraphsFeaturesSplitTextTest extends ParagraphsFeaturesJavascriptTestBa
     $this->assertSession()->assertWaitOnAjaxRequest();
     $page->find('xpath', '//*[contains(@class, "paragraphs-add-dialog") and contains(@class, "ui-dialog-content")]//*[contains(@name, "test_1")]')->click();
     $this->assertSession()->assertWaitOnAjaxRequest();
-    sleep(1);
 
     $ck_editor_id = $this->getCkEditorId($index);
     $driver->executeScript("CKEDITOR.instances['$ck_editor_id'].insertHtml('$text');");
@@ -49,7 +48,6 @@ class ParagraphsFeaturesSplitTextTest extends ParagraphsFeaturesJavascriptTestBa
   protected function clickParagraphSplitButton($ck_editor_index) {
     $this->getSession()->executeScript("jQuery('.cke_button__splittext:nth($ck_editor_index)').trigger('click');");
     $this->assertSession()->assertWaitOnAjaxRequest();
-    usleep(5000);
   }
 
   /**
@@ -332,6 +330,7 @@ class ParagraphsFeaturesSplitTextTest extends ParagraphsFeaturesJavascriptTestBa
     );
 
     // And then original collapsed paragraph.
+    $this->scrollElementInView('[name=field_paragraphs_0_edit]');
     $page->pressButton('field_paragraphs_0_edit');
     $this->assertSession()->assertWaitOnAjaxRequest();
 
