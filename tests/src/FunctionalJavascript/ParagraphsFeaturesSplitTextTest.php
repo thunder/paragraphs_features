e6eddaaea4721520d3389e4b463966839fbe609a
Christian Fritsch <chr.fritsch@gmx.net>
2021-06-29 08:43:19 +0200

    fix: add sleep

--- tests/src/FunctionalJavascript/ParagraphsFeaturesSplitTextTest.php
+++ tests/src/FunctionalJavascript/ParagraphsFeaturesSplitTextTest.php
@@ -32,6 +32,7 @@ class ParagraphsFeaturesSplitTextTest extends ParagraphsFeaturesJavascriptTestBa
     $this->assertSession()->assertWaitOnAjaxRequest();
     $page->find('xpath', '//*[contains(@class, "paragraphs-add-dialog") and contains(@class, "ui-dialog-content")]//*[contains(@name, "test_1")]')->click();
     $this->assertSession()->assertWaitOnAjaxRequest();
+    sleep(1);
 
     $ck_editor_id = $this->getCkEditorId($index);
     $driver->executeScript("CKEDITOR.instances['$ck_editor_id'].insertHtml('$text');");
