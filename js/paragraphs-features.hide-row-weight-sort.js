3c3f806601a66f0d56b5d639dff866e1bf61ad4b
Volker Killesreiter <killesre@gmail.com>
2021-10-01 11:12:06 +0200

    Add hide row weight sort option

---
+++ js/paragraphs-features.hide-row-weight-sort.js
@@ -0,0 +1,21 @@
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.paragraphsFeaturesHideRowWeightSort = {
+    attach: function (context, settings) {
+
+      Object.keys(settings.paragraphs_features.hide_row_weight_sort).forEach( function (paragraphsWidgetId) {
+        var wrapper = document.querySelector('#' + paragraphsWidgetId);
+        if (!wrapper) {
+          return;
+        }
+        var table = wrapper.querySelector('.field-multiple-table');
+        if (table && Drupal.tableDrag[table.id] && !Drupal.tableDrag[table.id].hideRowWeightSort) {
+          Drupal.tableDrag[table.id].$toggleWeightButton.hide();
+          Drupal.tableDrag[table.id].hideRowWeightSort = true;
+        }
+      });
+    }
+  };
+}(jQuery, Drupal));
