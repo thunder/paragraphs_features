94db11105f9c7f56ca1b7f510bc2434980e2a69d
Volker Killesreiter <killesre@gmail.com>
2021-11-05 14:54:11 +0100

    refact add in between

--- js/paragraphs-features.add-in-between.js
+++ js/paragraphs-features.add-in-between.js
@@ -2,9 +2,9 @@
  * @file thunder-paragraph-features.add-in-between.js
  */
 
-(function ($, Drupal, drupalSettings) {
+(($, Drupal, once) => {
 
-  'use strict';
+  // 'use strict';
 
   /**
    * Ensure namespace for paragraphs features exists.
@@ -26,108 +26,96 @@
    * @param {object} config
    *   Configuration options for add in between row template.
    *
-   * @return {string}
-   *   Returns markup string for add in between row.
+   * @return {HTMLElement}
+   *   Returns element for add in between row.
    */
-  Drupal.theme.paragraphsFeaturesAddInBetweenRow = function (config) {
-    return '' +
-      '<tr class="paragraphs-features__add-in-between__row">' +
-      '  <td colspan="100%">' +
-      '    <div class="paragraphs-features__add-in-between__wrapper">' +
-      '      <input class="paragraphs-features__add-in-between__button button--small js-show button js-form-submit form-submit" type="submit" value="' + config.text + '">' +
-      '    </div>' +
-      '  </td>' +
-      '</tr>';
+  Drupal.theme.paragraphsFeaturesAddInBetweenRow = (config) => {
+    const input = document.createElement('input'),
+      wrapper = document.createElement('div'),
+      td = document.createElement("td"),
+      row = document.createElement('tr');
+
+    input.classList.add('paragraphs-features__add-in-between__button', 'button--small', 'js-show', 'button', 'js-form-submit', 'form-submit');
+    input.type = 'submit';
+    input.value = config.text;
+    wrapper.classList.add('paragraphs-features__add-in-between__wrapper');
+    wrapper.appendChild(input);
+    td.setAttribute('colspan', '100%')
+    td.appendChild(wrapper);
+    row.classList.add('paragraphs-features__add-in-between__row');
+    row.appendChild(td);
+
+    return row;
   };
 
   /**
-   * Init add in between buttons for paragraphs table.
+   * Attach add in between buttons on paragraphs table.
    *
-   * @type {Object}
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   *   Attaches the behavior.
    */
   Drupal.behaviors.paragraphsFeaturesAddInBetweenInit = {
-    attach: function () {
-      $.each(drupalSettings.paragraphs_features.add_in_between, function (paragraphsWidgetId) {
-        Drupal.paragraphs_features.add_in_between.initParagraphsWidget(paragraphsWidgetId);
+    attach: (context, settings) => {
+      Object.values(settings.paragraphs_features.add_in_between || {}).forEach(function (field) {
+        Drupal.paragraphs_features.add_in_between.initParagraphsWidget(context, field);
       });
     }
-  };
-
-  /**
-   * Get paragraphs add modal block in various themes structures.
-   *
-   *  gin:
-   *   .layer-wrapper table
-   *   .form-actions
-   * claro:
-   *   table
-   *   .form-actions
-   * thunder-admin / seven:
-   *   table
-   *   .clearfix
-   *
-   * @param {Object} $table
-   *   jQuery object for the table element.
-   *
-   * @return {Object} $addModalBlock
-   *   jQuery object for the add modal block element.
-   */
-  Drupal.paragraphs_features.add_in_between.getAddModalBlock = function ($table) {
-    var $addModalBlock = $table.siblings('.form-actions, .clearfix');
-    if ($addModalBlock.length === 0) {
-      $addModalBlock = $table.parents('.layer-wrapper').first().siblings('.form-actions');
-    }
-    return $addModalBlock;
   };
 
   /**
    * Init paragraphs widget with add in between functionality.
    *
-   * @param {string} paragraphsWidgetId
+   * @param {array} paragraphsWidgetId
    *   Paragraphs Widget ID.
    */
-  Drupal.paragraphs_features.add_in_between.initParagraphsWidget = function (paragraphsWidgetId) {
-    var $tables = $('#' + paragraphsWidgetId).find('.field-multiple-table').first()
-      .once('paragraphs-features-add-in-between-init');
+  Drupal.paragraphs_features.add_in_between.initParagraphsWidget = function (context, field) {
+    const [wrapper] = once('paragraphs-features-add-in-between-init', '#' + field.wrapperId, context);
 
-    $tables.each(function (index, table) {
-      var $table = $(table);
-      var $addModalBlock = Drupal.paragraphs_features.add_in_between.getAddModalBlock($table);
-      var $addModalButton = $addModalBlock.find('.paragraph-type-add-modal-button');
+    if (!wrapper) return;
 
-      // Ensure that paragraph list uses modal dialog.
-      if ($addModalButton.length === 0) {
-        return;
-      }
+    const table = wrapper.querySelector('.field-multiple-table'),
+          addModalBlock = wrapper.querySelector('.paragraphs-add-wrapper'),
+          addModalButton = addModalBlock.querySelector('.paragraph-type-add-modal-button');
 
-      // A new button for adding at the end of the list is used.
-      $addModalBlock.hide();
+    // Ensure that paragraph list uses modal dialog.
+    if (!addModalButton) {
+      return;
+    }
 
-      var rowMarkup = Drupal.theme('paragraphsFeaturesAddInBetweenRow', {text: Drupal.t('+ Add')});
+    // A new button for adding at the end of the list is used.
+    addModalBlock.style.display = 'none';
 
-      // Add buttons and adjust drag-drop functionality.
-      var $tableBody = $table.find('> tbody');
-      $tableBody.find('> tr').each(function (index, rowElement) {
-        $(rowMarkup).insertBefore(rowElement);
-      });
+    const buttonRowElement = () => {
+      return Drupal.theme('paragraphsFeaturesAddInBetweenRow', {text: Drupal.t('+ Add')});
+    }
 
-      // Add a new button for adding a new paragraph to the end of the list.
-      if ($tableBody.length === 0) {
-        $table.append('<tbody></tbody>');
+    // Add buttons and adjust drag-drop functionality.
+    let tableBody = table.querySelector(':scope > tbody');
 
-        $tableBody = $table.find('> tbody');
-      }
-      $tableBody.append(rowMarkup);
+    // Add a new button for adding a new paragraph to the end of the list.
+    if (!tableBody) {
+      table.insertAdjacentHTML('beforeend', '<tbody></tbody>');
 
-      // Adding of a new paragraph can be disabled for some reason.
-      if ($addModalButton.is(':disabled')) {
-        $tableBody.find('.paragraphs-features__add-in-between__button')
-          .prop('disabled', 'disabled').addClass('is-disabled');
-      }
+      tableBody = table.querySelector(':scope > tbody');
+    }
 
-      // Trigger attaching of behaviours for added buttons.
-      Drupal.behaviors.paragraphsFeaturesAddInBetweenRegister.attach($table);
+    tableBody.querySelectorAll(':scope > tr').forEach((rowElement) => {
+      rowElement.insertAdjacentElement('beforebegin', buttonRowElement());
     });
+    tableBody.appendChild(buttonRowElement());
+
+    // Adding of a new paragraph can be disabled for some reason.
+    if (addModalButton.getAttribute('disabled')) {
+      tableBody.querySelectorAll('.paragraphs-features__add-in-between__button').forEach((button) => {
+        button.setAttribute('disabled', 'disabled');
+        button.classList.add('is-disabled');
+      });
+    }
+
+    // Trigger attaching of behaviours for added buttons.
+    Drupal.behaviors.paragraphsFeaturesAddInBetweenRegister.attach(table);
   };
 
   /**
@@ -136,39 +124,37 @@
    * @type {Object}
    */
   Drupal.behaviors.paragraphsFeaturesAddInBetweenRegister = {
-    attach: function (context) {
-      $('.paragraphs-features__add-in-between__button', context)
-        .once('paragraphs-features-add-in-between')
-        .on('click', function (event) {
-          var $button = $(this);
-          var $add_more_wrapper = Drupal.paragraphs_features.add_in_between.getAddModalBlock($button.closest('table'))
-            .find('.paragraphs-add-dialog');
-          var delta = $button.closest('tr').index() / 2;
+    attach: (context) => {
+      once('paragraphs-features-add-in-between', '.paragraphs-features__add-in-between__button', context).forEach((button) => {
+        button.addEventListener('click', (event) => {
+          const dialog = button.closest('table').parentNode.querySelector('.paragraphs-add-wrapper').querySelector('.paragraphs-add-dialog');
+          const row = button.closest('tr');
+          const delta = Array.prototype.indexOf.call(row.parentNode.children, row) / 2;
 
           // Set delta before opening of dialog.
-          Drupal.paragraphs_features.add_in_between.setDelta($add_more_wrapper, delta);
-          Drupal.paragraphsAddModal.openDialog($add_more_wrapper, Drupal.t('Add In Between'));
+          Drupal.paragraphs_features.add_in_between.setDelta(dialog, delta);
+          Drupal.paragraphsAddModal.openDialog(dialog, Drupal.t('Add In Between'));
 
           // Stop default execution of click event.
           event.preventDefault();
           event.stopPropagation();
         });
+      });
     }
   };
 
   /**
    * Set delta into hidden field, where a new paragraph will be added.
    *
-   * @param {Object} $add_more_wrapper
+   * @param {Object} dialog
    *   jQuery object for add more wrapper element.
    * @param {int} delta
    *   Integer value for delta position where a new paragraph should be added.
    */
-  Drupal.paragraphs_features.add_in_between.setDelta = function ($add_more_wrapper, delta) {
-    var $delta = $add_more_wrapper.parents('.form-actions, .clearfix')
-      .find('.paragraph-type-add-modal-delta');
+  Drupal.paragraphs_features.add_in_between.setDelta = (dialog, delta) => {
+    let deltaInput = dialog.closest('.paragraphs-add-wrapper').querySelector('.paragraph-type-add-modal-delta');
 
-    $delta.val(delta);
+    deltaInput.value = delta;
   };
 
   /**
@@ -177,18 +163,17 @@
    * @type {Object}
    */
   Drupal.behaviors.paragraphsFeaturesAddInBetweenTableDragDrop = {
-    attach: function () {
-      for (var tableId in drupalSettings.tableDrag) {
-        if (Object.prototype.hasOwnProperty.call(drupalSettings.tableDrag, tableId)) {
+    attach: (context, settings) => {
+      for (const tableId in settings.tableDrag) {
+        if (Object.prototype.hasOwnProperty.call(settings.tableDrag, tableId)) {
           Drupal.paragraphs_features.add_in_between.adjustDragDrop(tableId);
-
-          jQuery('#' + tableId)
-            .once('in-between-buttons-columnschange')
-            .on('columnschange', function () {
-              var tableId = $(this).prop('id');
-
-              Drupal.paragraphs_features.add_in_between.adjustDragDrop(tableId);
+          // Show / hide row weights.
+          once('in-between-buttons-columnschange', '#' + tableId, context).forEach((table) => {
+            // drupal tabledrag uses jquery events.
+            $(table).on('columnschange', () => {
+              Drupal.paragraphs_features.add_in_between.adjustDragDrop(this.id);
             });
+          });
         }
       }
     }
@@ -201,7 +186,7 @@
    * @param {string} tableId
    *   Table ID for paragraphs table with adjusted drag-drop behaviour.
    */
-  Drupal.paragraphs_features.add_in_between.adjustDragDrop = function (tableId) {
+  Drupal.paragraphs_features.add_in_between.adjustDragDrop = (tableId) => {
     // Ensure that function changes are executed only once.
     if (!Drupal.tableDrag[tableId] || Drupal.tableDrag[tableId].paragraphsDragDrop) {
       return;
@@ -209,9 +194,10 @@
     Drupal.tableDrag[tableId].paragraphsDragDrop = true;
 
     // Helper function to create sequence execution of two bool functions.
-    var sequenceBoolFunctions = function (originalFn, newFn) {
-      return function () {
-        var result = originalFn.apply(this, arguments);
+    const sequenceBoolFunctions = (originalFn, newFn) => {
+      // Arrow functions do not support arguments.
+      return function() {
+        let result = originalFn.apply(this, arguments);
 
         if (result) {
           result = newFn.apply(this, arguments);
@@ -222,27 +208,27 @@
     };
 
     // Allow row swap if it's not in between button.
-    var paragraphsIsValidSwap = function (row) {
-      return !$(row).hasClass('paragraphs-features__add-in-between__row');
+    const paragraphsIsValidSwap = (row) => {
+      return !row.classList.contains('paragraphs-features__add-in-between__row');
     };
 
     // Sequence default .isValidSwap() function with custom paragraphs function.
-    var rowObject = Drupal.tableDrag[tableId].row;
+    const rowObject = Drupal.tableDrag[tableId].row;
     rowObject.prototype.isValidSwap = sequenceBoolFunctions(rowObject.prototype.isValidSwap, paragraphsIsValidSwap);
 
     // provide custom .onSwap() handler to reorder "Add" buttons.
-    rowObject.prototype.onSwap = function (row) {
-      var $table = $(row).closest('table');
-      var allDrags = $table.find('> tbody > tr.draggable');
-      var allAdds = $table.find('> tbody > tr.paragraphs-features__add-in-between__row');
+    rowObject.prototype.onSwap = (row) => {
+      const table = row.closest('table'),
+        allDrags = table.querySelectorAll(':scope > tbody > tr.draggable'),
+        allAdds = table.querySelectorAll(':scope > tbody > tr.paragraphs-features__add-in-between__row');
 
       // We have to re-stripe add in between rows.
-      allDrags.each(function (index, dragElem) {
-        if (allAdds[index]) {
-          $(dragElem).before(allAdds[index]);
+      allDrags.forEach( (dragElem, index) => {
+        if (allAdds.item(index)) {
+          dragElem.insertAdjacentElement('beforebegin', allAdds.item(index));
         }
       });
     };
   };
 
-}(jQuery, Drupal, drupalSettings));
+})(jQuery, Drupal, once);
