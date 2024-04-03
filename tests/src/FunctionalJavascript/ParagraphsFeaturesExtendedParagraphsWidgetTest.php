9245e381532b726f3e10c804c7325c748f4d3994
Volker Killesreiter <killesre@gmail.com>
2024-04-03 15:24:04 +0200

    fix claro

--- tests/src/FunctionalJavascript/ParagraphsFeaturesExtendedParagraphsWidgetTest.php
+++ tests/src/FunctionalJavascript/ParagraphsFeaturesExtendedParagraphsWidgetTest.php
@@ -60,7 +60,6 @@ class ParagraphsFeaturesExtendedParagraphsWidgetTest extends ParagraphsFeaturesJ
     // Add a nested paragraph and check that add in between is used only for
     // base paragraphs field, but not for the nested paragraph.
     $session->executeScript("document.querySelector('.paragraphs-features__add-in-between__button').click()");
-    $this->assertSession()->assertWaitOnAjaxRequest();
     $page->find('xpath', '//*[contains(@class, "paragraphs-add-dialog") and contains(@class, "ui-dialog-content")]//*[contains(@name, "test_nested")]')->click();
     $this->assertSession()->assertWaitOnAjaxRequest();
 
