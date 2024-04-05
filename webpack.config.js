36709d1b68d92f88ce743865bf9823216a6908ad
Volker Killesreiter <killesre@gmail.com>
2024-04-05 11:50:25 +0200

    fix cs

--- webpack.config.js
+++ webpack.config.js
@@ -36,8 +36,8 @@ getDirectories('./js/ckeditor5_plugins').forEach((dir) => {
         __dirname,
         'js/ckeditor5_plugins',
         dir,
-        'src/index.js',
-      ),
+        'src/index.js'
+      )
     },
     output: {
       path: path.resolve(__dirname, './js/build'),
